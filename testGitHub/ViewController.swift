ad2560ef47c4525ba3152799d98c9e2ecc34db37
ming hoe <XCODELEARNING@GMAIL.COM>
2018-01-31 16:47:20 +0800

    first

---
+++ testGitHub/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  testGitHub
+//
+//  Created by ming hoe on 31/01/2018.
+//  Copyright © 2018 ming hoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        ONETWOTHREEEEEEEEEEEEEEEEE
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
